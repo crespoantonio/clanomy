83c32865892189f9848a45c61e9b850c0de96f36
Antonio Crespo <lic.crespoantonio@gmail.com>
2026-09-03 09:50:56 -0300

    Update to see if we can see logs

--- alembic/env.py
+++ alembic/env.py
@@ -20,7 +20,7 @@ config = context.config
 
 # Interpret the config file for Python logging.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 # Set target metadata from SQLModel
 target_metadata = SQLModel.metadata
